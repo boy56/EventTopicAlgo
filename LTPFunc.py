42f18c41eb4ad7779108cc534e20bebf2558a2ba
boy56 <duanjian56@126.com>
2020-09-10 16:02:02 +0800

    ==

--- LTPFunc.py
+++ LTPFunc.py
@@ -33,15 +33,15 @@ class LTPFunction:
         # postagger.release()
         return [i for i in words_postags]
 
-    # 实体抽取, 输入为分词列表、词性标注列表, 输出为人名列表、地名列表、机构名列表
+    # 实体抽取, 输入为分词列表、词性标注列表, 输出为人名集合、地名集合、机构名集合
     def ltp_ner(self, word_list, words_postags):
         netags = self.recognizer.recognize(word_list, words_postags)
         # print(" ".join(netags))
         entity = ''
         tag = ''
-        person_list = set()
-        location_list = set()
-        organization_list = set()
+        person_set = set()
+        location_set = set()
+        organization_set = set()
         for i in range(len(netags)):
             ner = netags[i].split('-')
             if ner[0] == 'O':
@@ -78,7 +78,7 @@ class LTPFunction:
                     organization_list.add(entity)
                 entity = ''
                 tag = ''
-        return list(person_list), list(location_list), list(organization_list)
+        return person_set, location_set, organization_set
 
     # 句法分析, 输入为分词列表、词性标注列表, 输出为关系(Relation)列表、父节点(Head)列表
     def ltp_parser(self, word_list, pos_list):
@@ -100,12 +100,17 @@ class LTPFunction:
         organization_set = set()
 
         # 根据title获取对应的信息
-        title_words = ltpFunction.ltp_seg("原创航行自由换人了?美海岸警卫队接替美海军巡航,目前已闯进黄海。")
-        title_pos = ltpFunction.ltp_pos(title_words)
-        p_list, l_list, o_list = ltpFunction.ltp_ner(title_words, title_pos)
+        title_words = self.ltp_seg(title)
+        title_pos = self.ltp_pos(title_words)
+        p_set, l_set, o_set = ltpFunction.ltp_ner(title_words, title_pos)
 
+        # 将获取的集合放入结果中
+        person_set = person_set | p_set
+        location_set = location_set | l_set
+        organization_set = organization_set | o_set
+        
         # 根据content获取对应的信息 (是否分句后再处理)
-
+        
 
         return list(person_set), list(location_set), list(organization_set)
 
