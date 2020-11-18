31d999d8de203ff11a73f43a7fe304c9ed3cc2d4
boy56 <duanjian56@126.com>
2020-11-18 16:04:34 +0800

    ==

--- EventPredictFunc.py
+++ EventPredictFunc.py
@@ -27,41 +27,56 @@ class EventPredictFunc:
     # 通过新闻title计算后续可能发生的事件概率
     def calcu_next_event(self, theme, title):
         theme_ep_dict = self.ep_words_dict[theme]
-        wordpair_ep_dict= {} # 词对触发的候选事件
-        wordpair_w_dict = {} # 词对对候选事件的贡献度
+        # wordpair_ep_dict= {} # 词对触发的候选事件
+        # wordpair_w_dict = {} # 词对对候选事件的贡献度
+        wordpair_event_weight_dict = {} # {(wordpair):{nextevent: weight}}
 
         # 根据字典构造wordpair_ep_dict 和 wordpair_w_dict
         for next_event, condition_list in theme_ep_dict.items():
             for word_str_dict in condition_list:
                 for word_str, weight in word_str_dict.items(): # 一般只有一个词条
-                    
                     data_list = []
                     and_word_list = word_str.replace(" ","").split(',') # 先去除空格再划分
                     for or_words in and_word_list:
                         data_list.append(or_words.split('/'))
                     # 使用笛卡尔积来组合词条
                     for item in itertools.product(*data_list):
-                        wordpair_ep_dict[item] = next_event
-                        wordpair_w_dict[item] = weight
+                        # wordpair_ep_dict[item] = next_event
+                        # wordpair_w_dict[item] = weight
+                        if item not in wordpair_event_weight_dict:
+                            wordpair_event_weight_dict[item] = {}
+                        wordpair_event_weight_dict[item][next_event] = weight
 
         # print(wordpair_ep_dict)
         # print(wordpair_w_dict)
+        # print(wordpair_event_weight_dict)
 
         result = {} # 对候选事件的计算结果
         # 根据theme_ep_dict构造{词对:候选者事件}, 用每个词对匹配title获取其对候选事件的触发值
-        for wordpair in wordpair_ep_dict.keys():
-            # print(wordpair)
-            flag = True
-            next_event = wordpair_ep_dict[wordpair]
-            weight = wordpair_w_dict[wordpair]
-            for w in wordpair:
+        # for wordpair in wordpair_ep_dict.keys():
+        #     # print(wordpair)
+        #     flag = True
+        #     next_event = wordpair_ep_dict[wordpair]
+        #     weight = wordpair_w_dict[wordpair]
+        #     for w in wordpair:
+        #         if w not in title:
+        #             flag = False
+        #             break
+        #     if flag: 
+        #         result[next_event] = weight
+        #         break # 一个title默认只匹配一个词条
+        
+        # 每个title可以出发多个词对，每个词对可以导致多个后续事件
+        for wp, nextevents in wordpair_event_weight_dict.items():
+            flag = True # 判断是否符合词对条件
+            for w in wp:
                 if w not in title:
                     flag = False
                     break
-            if flag: 
-                result[next_event] = weight
-                break # 一个title默认只匹配一个词条
-        
+            if flag:
+                for next_event, weight in nextevents.items(): 
+                    result[next_event] = weight
+
         # 如果没有候选事件则补无风险事件
         if not result:
             result['无风险事件'] = 1
